31d185bbe97f7138bd57d97a4a09d738d050c703
rikcarve <rik@carve.ch>
2017-01-10 21:35:00 +0100

    add possibility to override service discovery with env variable

--- src/main/java/ch/carve/consul/discovery/ServiceDiscovery.java
+++ src/main/java/ch/carve/consul/discovery/ServiceDiscovery.java
@@ -1,6 +1,8 @@
 package ch.carve.consul.discovery;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -15,14 +17,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Service discovery which provides a producer for {@link ServiceUriProvider}'s
- * Injected backend
+ * Service discovery which provides a producer for {@link ServiceUriProvider}'s.
+ * Injected {@link ServiceDiscoveryBackend}.
+ * 
  */
 @Singleton
 public class ServiceDiscovery {
     private static final Logger logger = LoggerFactory.getLogger(ServiceDiscovery.class);
 
     private Map<String, List<String>> services = new ConcurrentHashMap<>();
+    private Map<String, List<String>> envOverrides = new ConcurrentHashMap<>();
 
     @Inject
     private ServiceDiscoveryBackend backend;
@@ -38,6 +42,13 @@ public class ServiceDiscovery {
     public ServiceUriProvider getServiceUriProvider(InjectionPoint ip) {
         logger.info("produce service provider");
         String serviceName = ip.getAnnotated().getAnnotation(DiscoverableService.class).serviceName();
+
+        // check for environment overrides
+        List<String> envHosts = checkEnv(serviceName);
+        if (!envHosts.isEmpty()) {
+            return new ServiceUriProvider(serviceName, envHosts);
+        }
+
         List<String> hosts = services.get(serviceName);
         if (isNullOrEmpty(hosts)) {
             hosts = backend.getUpdatedListOfServers(serviceName);
@@ -47,6 +58,21 @@ public class ServiceDiscovery {
             services.put(serviceName, hosts);
         }
         return new ServiceUriProvider(serviceName, hosts);
+    }
+
+    private List<String> checkEnv(String serviceName) {
+        List<String> envHosts = envOverrides.get(serviceName);
+        if (envHosts == null) {
+            String env = System.getenv(serviceName + "-nodes");
+            if (env != null) {
+                logger.info("Environment override: using {} for service {}", env, serviceName);
+                envHosts = Arrays.asList(env.split(","));
+            } else {
+                envHosts = Collections.emptyList();
+            }
+            envOverrides.put(serviceName, envHosts);
+        }
+        return envHosts;
     }
 
     private static boolean isNullOrEmpty(final Collection<?> c) {
